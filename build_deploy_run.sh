7a53629280c3f9c01cef9fc0d59ced40c7b95b22
nreese <reese.nathan@gmail.com>
2016-04-21 17:02:16 -0600

    add script to build, install, and run plugin

---
+++ build_deploy_run.sh
@@ -0,0 +1,16 @@
+LOGSTASH_HOME="../logstash-2.2.2"
+
+echo "## Uninstalling existing logstash-input-mongodb plugin"
+$LOGSTASH_HOME/bin/plugin uninstall logstash-input-mongodb
+
+echo "## Building logstash-input-mongodb"
+rm logstash-input-mongodb-*.gem
+gem build logstash-input-mongodb.gemspec
+
+echo "## Deploying logstash-input-mongodb"
+$LOGSTASH_HOME/bin/plugin install logstash-input-mongodb-0.4.0.gem
+
+echo "## Starting logstash with test configuration"
+cat scripts/test.conf
+rm logstash-mongodb/logstash_sqlite.db
+$LOGSTASH_HOME/bin/logstash -f scripts/test.conf
